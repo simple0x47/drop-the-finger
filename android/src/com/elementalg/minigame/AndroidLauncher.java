a96cce975760a6c3512bdffe9718fb3e8dc4888e
Gabriel Amihalachioaie <gamihalachioaie@gmail.com>
2020-09-10 21:43:12 +0200

    LibGDX setup.

---
+++ android/src/com/elementalg/minigame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.elementalg.minigame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.elementalg.minigame.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/elementalg/minigame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.elementalg.minigame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.elementalg.minigame.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
